564329e31283d3e89ae884c83b8159b7e7b6b54f
nansant <renan.2r@live.com>
2025-02-19 10:57:34 -0300

    Adicionado formulário de cadastro funcional e integrado com API

--- src/app/components/pages/cadastro-contatos/cadastro-contatos.component.ts
+++ src/app/components/pages/cadastro-contatos/cadastro-contatos.component.ts
@@ -1,11 +1,42 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro-contatos',
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './cadastro-contatos.component.html',
-  styleUrl: './cadastro-contatos.component.css'
+  styleUrl: './cadastro-contatos.component.css',
 })
 export class CadastroContatosComponent {
+  form = new FormGroup({
+    nome: new FormControl(''),
+    telefone: new FormControl(''),
+    email: new FormControl(''),
+  });
 
+  constructor(private http: HttpClient) {}
+
+  resposta: string = '';
+
+  OnSubmit() {
+    this.http
+      .post('http://localhost:8080/api/contatos/cadastrar', this.form.value, {
+        responseType: 'text',
+      })
+      .subscribe({
+        next: (data) => {
+          this.resposta = data;
+        },
+        error: (e) => {
+          console.log(e.error);
+        },
+      });
+  }
 }
